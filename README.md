a5462f028aede371a282d3eca222089697e379f1
vs <valentin@vs-coding.de>
2014-06-17 08:57:19 +0200

    update

--- README.md
+++ README.md
@@ -3,3 +3,7 @@ multi_playlist
 
 just plays every audio from soundcloud embedded on your page.
 
+    <script language="javascript" type="text/javascript" src="https://w.soundcloud.com/player/api.js"></script>
+    <script language="javascript" type="text/javascript" src="https://raw.githubusercontent.com/vs-de/multi_playlist/master/mpl.js"></script>
+
+
--- mpl.js
+++ mpl.js
@@ -1,11 +1,12 @@
-var pl = $('iframe').filter(function(i, e){
-  return $(e).attr('src') ? $(e).attr('src').match(/soundcloud/) : false
+$(document).ready(function {
+  var pl = $('iframe').filter(function(i, e){
+    return $(e).attr('src') ? $(e).attr('src').match(/soundcloud/) : false
+  });
+  pl.each(function(i,e){
+    SC.Widget(e).bind(
+      SC.Widget.Events.FINISH, 
+      function(event, data){
+        SC.Widget(my_arr[(i+1) % pl.size()]).play();
+      })
+  });
 });
-pl.each(function(i,e){
-  SC.Widget(e).bind(
-    SC.Widget.Events.FINISH, 
-    function(event, data){
-      SC.Widget(my_arr[(i+1) % pl.size()]).play();
-    })
-});
-
